bda4121efd342efe1fc678e1af8eaedfd63fe608
0fflines <6182201102@student.unpar.ac.id>
2025-11-18 10:53:58 +0700

    init upload project template

---
+++ src/main/java/com/example/tubesIoT/TubesIoTApplication.java
@@ -0,0 +1,13 @@
+package com.example.tubesIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tubesIoT/TubesIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tubesIoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
